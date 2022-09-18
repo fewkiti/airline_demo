22ee93817f28785cb30c2e55e33cc268e0b6e64d
weerachai <anweerac@tu.ac.th>
2022-09-18 10:42:09 +0000

    Sun, Sep 18, 2022, 10:42 AM +00:00

--- flights/test_views.py
+++ flights/test_views.py
@@ -29,7 +29,6 @@ class FlightViewTestCase(TestCase):
 
         c = Client()
         response = c.get(reverse('flights:index'))
-        print(response.context['flights'])
         self.assertEqual(
             response.context['flights'].count(), 1)
 
