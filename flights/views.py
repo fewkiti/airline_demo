499d9fde55672c66ab1c426d2c21f0c3bccd9e8e
weerachai <anweerac@tu.ac.th>
2022-09-18 10:34:43 +0000

    Sun, Sep 18, 2022, 10:34 AM +00:00

--- flights/views.py
+++ flights/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -11,7 +11,7 @@ def index(request):
     })
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(pk=flight_id)
+    flight = Flight.objects.get_object_or_404(pk=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
         "passengers": flight.passengers.all(),
