d1c28b5322bc42b11fa7d2d7d0a8cddf97c3d816
weerachai <anweerac@tu.ac.th>
2022-09-18 09:48:33 +0000

    Sun, Sep 18, 2022, 9:48 AM +00:00

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Airport(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
@@ -8,18 +10,27 @@ class Airport(models.Model):
     def __str__(self):
         return f"{self.city} ({self.code})"
 
+
 class Flight(models.Model):
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    origin = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+    capacity = models.PositiveIntegerField(default=2)
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
 
+    def is_seat_available(self):
+        return self.passengers.count() < self.capacity
+
+
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
-    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+    flights = models.ManyToManyField(
+        Flight, blank=True, related_name="passengers")
 
     def __str__(self):
         return f"{self.first} {self.last}"
--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Airport, Flight, Passenger
 
-# Create your tests here.
+
+class FlightTestCase(TestCase):
+
+    def setUp(self):
+
+        # create airports
+        airport1 = Airport.objects.create(code="AAA", city="City A")
+        airport2 = Airport.objects.create(code="BBB", city="City B")
+
+        Flight.objects.create(
+            origin=airport1, destination=airport2, duration=400)
+
+    def test_seat_available(self):
+        """ is_seat_available should be True """
+
+        flight = Flight.objects.first()
+
+        self.assertTrue(flight.is_seat_available())
+
+    def test_seat_not_available(self):
+        """ is_seat_available should be False """
+
+        passenger1 = Passenger.objects.create(
+            first="harry", last="potter")
+        passenger2 = Passenger.objects.create(
+            first="hermione", last="granger")
+
+        flight = Flight.objects.first()
+        flight.passengers.add(passenger1)
+        flight.passengers.add(passenger2)
+
+        self.assertFalse(flight.is_seat_available())
